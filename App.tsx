1ed09c05983a322f88cd82c46ccbb2805d378124
Kamva-pro <kamvelihlefatman@gmail.com>
2025-01-17 22:54:21 +0200

    added paper provider

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+
 import Home from './pages/HomePage';
 import Menu from './pages/Menu';
 
@@ -10,11 +12,13 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <PaperProvider>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Menu" component={Menu} />
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   )
 }
