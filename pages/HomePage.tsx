5d3078f39ba1e61a499a1e54fc579d31e4db6bb9
Kamva-pro <kamvelihlefatman@gmail.com>
2025-01-18 08:18:31 +0200

    refactored home to relevant typescript

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,14 +1,44 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { FAB } from "react-native-paper";
+import { StackNavigationProp } from "@react-navigation/stack";
 
+type RootStackParamList = {
+  Home: undefined; // No params for Home screen
+  Menu: undefined; // No params for Menu screen
+};
 
-const Home = () => {
-    return(
-        <View>
-            
-        </View>
-    )
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, "Home">;
 
-}
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
 
-export default Home
+const Home: React.FC<Props> = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        label="Add"
+        onPress={() => navigation.navigate("Menu")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fab: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#6200ee",
+  },
+});
+
+export default Home;
